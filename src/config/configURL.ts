d07d29ff4ac6db2f55348e695c4d15f2fc333f85
Rahi-007 <linnima072@gmail.com>
2026-03-13 23:28:36 +0600

    err fixed

--- src/config/configURL.ts
+++ src/config/configURL.ts
@@ -1,11 +1,20 @@
 import { BASE_URL } from "./const";
 
+export const api = (l?: string) => {
+  return l ? `${BASE_URL}/api/${l}` : BASE_URL;
+};
+
+function objectToUrlParams(obj: Record<string, any>): string {
+  return new URLSearchParams(obj).toString();
+}
+
 export const API_URLS = {
-  books: {
-    getAll: `${BASE_URL}/books`,
-    getById: (id: number) => `${BASE_URL}/books/${id}`,
-    create: `${BASE_URL}/books`,
-    update: (id: number) => `${BASE_URL}/books/${id}`,
-    delete: (id: number) => `${BASE_URL}/books/${id}`,
+  user: {
+    all: (s?: Record<string, any>) =>  api(["user", objectToUrlParams(s || {})].join("?")),
+    add: () => api("user"),
+    get: (id: number) => api(`user/${id}`),
+    create: () => api("user"),
+    update: (id: number) => api(`user/${id}`),
+    delete: (id: number) => api(`user/${id}`),
   },
 };
